70f1398b362c5713d36f4bd1a05dbb3ac9d38121
lielsmoon <baluzaveta@gmail.com>
2022-02-03 15:20:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header>
+                <img
+                    src="https://cdn.logo.com/hotlink-ok/logo-social.png"
+                    alt="logo"
+                />
+            </header>
+            <nav>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>Main content</div>
+            </nav>
+        </div>
+    );
 }
 
 export default App;
